aa10910962a4a0e3620e4df56f5997a670200d12
Geet Sethi <geetsethi05@gmail.com>
2021-05-08 12:09:48 +0530

    🎉 Initial commit

---
+++ graph/resolver/resolver.go
@@ -0,0 +1,12 @@
+// Package resolver provide the resolver struct for
+// resolving graphql queries and mutations
+// The Resolver struct also handles dependency injection for the resolvers
+package resolver
+
+//go:generate go run github.com/99designs/gqlgen
+import "github.com/sethigeet/gql-go-auth-backend/graph/model"
+
+type Resolver struct {
+	todos []*model.Todo
+	users []*model.User
+}
---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/sethigeet/gql-go-auth-backend/graph/generated"
+	"github.com/sethigeet/gql-go-auth-backend/graph/resolver"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	server := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
